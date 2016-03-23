1a8e16a30ee70e6372990cb621d897570103b0e8
Alex Holloway <x4nd3r@users.noreply.github.com>
2016-03-23 12:39:35 +1000

    Formatting, whitespace

--- server.R
+++ server.R
@@ -1,32 +1,30 @@
 shinyServer(function(input, output) {
   output$plot <- renderPlot({
-    p <- input$p
-    a <- input$a
-    d <- input$d
-    mu= a*(2*p - 1) + 2*d*p*(1-p)
-    beta= a + d*(1-2*p)
+    p  <- input$p
+    a  <- input$a
+    d  <- input$d
+    mu <- a*(2*p - 1) + 2*d*p*(1-p)
+    beta <- a + d*(1-2*p)
     # Genotype values
-    gAA= -a
-    gAB= d
-    gBB= a
-    varG = (1-p)^2 * (gAA-mu)^2 + 2*p*(1-p)*(gAB-mu)^2 + p^2*(gBB-mu)^2
-    varG
+    gAA  <- -a
+    gAB  <- d
+    gBB  <- a
+    varG <- (1-p)^2 * (gAA-mu)^2 + 2*p*(1-p)*(gAB-mu)^2 + p^2*(gBB-mu)^2
     # Breeding values for the 3 genotypes
-    bvAA= -2*p*beta
-    bvAB= (1-2*p)*beta
-    bvBB= 2*(1-p)*beta
-    varBV= (1-p)^2 * (bvAA)^2 + 2*p*(1-p)*(bvAB)^2 + p^2*(bvBB)^2
-    varBV
+    bvAA <- -2*p*beta
+    bvAB <- (1-2*p)*beta
+    bvBB <- 2*(1-p)*beta
+    varBV <- (1-p)^2 * (bvAA)^2 + 2*p*(1-p)*(bvAB)^2 + p^2*(bvBB)^2
     # Variance components
-    Va= 2*p*(1-p)*beta^2
-    Vd= (2*p*(1-p)*d)^2
-    Vg= Va + Vd
-    Va; Vd; Vg
+    Va <- 2*p*(1-p)*beta^2
+    Vd <- (2*p*(1-p)*d)^2
+    Vg <- Va + Vd
     # For plotting
-    x<- c(0,1,2)
-    y<- c(-a,d,a)
-    w<- c((1-p)^2, 2*p*(1-p),p^2)
-    cex.val = 1 + w
-    plot(x,y,cex=cex.val)
+    x <- c(0,1,2)
+    y <- c(-a,d,a)
+    w <- c((1-p)^2, 2*p*(1-p),p^2)
+    cex.val <- 1 + w
+    plot(x, y, cex = cex.val)
+    abline(lm(y ~ x))
   })
 })
