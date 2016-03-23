85ea75497ca654785c526fc5068f9e6c6bdac01b
Alex Holloway <x4nd3r@users.noreply.github.com>
2016-03-23 11:06:20 +1000

    Initialise

---
+++ ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Falconer additive-dominance model"),
+
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("p", "p",
+                  min = 0.0, max = 1.0, value = 0.3),
+      sliderInput("a", "a",
+                  min = 0.0, max = 10.0, value = 4.0),
+      sliderInput("d", "d",
+                  min = 0.0, max = 10.0, value = 2.0)
+    ),
+
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+))
